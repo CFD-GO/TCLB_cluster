95dea0c5b380262636eea0b7b63e66f5a9c2c423
Dmytro Sashko <dmshko@gmail.com>
2023-06-20 09:17:13 +1000

    Fix setonix config

--- cluster/setonix.bash
+++ cluster/setonix.bash
@@ -1,7 +1,7 @@
 # setonix.*
 
 echo
-echo "You are on the Topaz cluster (Pawsey, Australia)"
+echo "You are on the Setonix cluster (Pawsey, Australia)"
 echo
 
 fix ENGINE_CONF "local"
@@ -15,8 +15,8 @@ function RUN_GPU_CHECK {
 	y) def CONFOPT "--enable-hip --with-mpi-include=${CRAY_MPICH_DIR}/include --with-mpi-lib=${CRAY_MPICH_DIR}/lib" ;;
 	esac
 }
-fix DEBUG_PARTITION "debugq"
-fix MAIN_PARTITION "gpuq"
+fix DEBUG_PARTITION "gpu-dev"
+fix MAIN_PARTITION "gpu"
 fix MAX_TASKS_PER_NODE "2"
 fix MAX_TASKS_PER_NODE_FOR_COMPILATION "8"
 fix CORES_PER_TASK "1"
@@ -25,4 +25,5 @@ def MODULES_BASE "r/4.1.0 rocm/5.4.3"
 fix MODULES_ADD ""
 fix MODULES_RUN ""
 fix MODULES_CHECK_AVAILABILITY "y"
-
+fix RUN_COMMAND "srun"
+fix GRANT "${GRANT}-gpu"
